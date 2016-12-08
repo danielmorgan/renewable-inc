49c53cdb047c42c56d1922b81fa1080630584cff
Daniel Morgan <me@danielmorgan.co.uk>
2016-12-08 18:28:36 +0000

    Add the ability to sell joules for credits

--- src/Game.js
+++ src/Game.js
@@ -27,6 +27,13 @@ class Game extends Component {
     this.setState({ joules: this.state.joules + 1 });
   }
 
+  sellEnergy() {
+    this.setState({
+      credits: this.state.credits + this.state.joules,
+      joules: 0
+    });
+  }
+
   get duration() {
     return 60 / this.state.speed;
   }
@@ -52,16 +59,24 @@ class Game extends Component {
                 <tr>
                   <th>Unit</th>
                   <th>Amount</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td className="key">Credits</td>
                   <td>{this.state.credits}</td>
+                  <td></td>
                 </tr>
                 <tr>
                   <td className="key">Joules</td>
                   <td>{this.state.joules}</td>
+                  <td>
+                    <button className="btn btn-success"
+                            onClick={() => this.sellEnergy()}>
+                      Sell all
+                    </button>
+                  </td>
                 </tr>
               </tbody>
             </table>
